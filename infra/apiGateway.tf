ce8bfd2fd0cb1beaa043e67e486cd200021684b5
Manuel <jmpcba@gmail.com>
2019-07-26 00:48:28 -0300

    change deployment name

--- infra/apiGateway.tf
+++ infra/apiGateway.tf
@@ -42,5 +42,5 @@ resource "aws_api_gateway_deployment" "databroker_prod_deployment" {
   depends_on = ["aws_api_gateway_integration.databroker_get_integration"]
 
   rest_api_id = "${aws_api_gateway_rest_api.HC_REST_API.id}"
-  stage_name  = "PROD"
+  stage_name  = "prod"
 }
