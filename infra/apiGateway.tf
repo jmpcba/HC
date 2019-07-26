ab5c5673b98a856f00895db615fcd241bc2c36ff
Manuel <jmpcba@gmail.com>
2019-07-25 22:37:39 -0300

    add prod deployment

--- infra/apiGateway.tf
+++ infra/apiGateway.tf
@@ -37,3 +37,11 @@ resource "aws_api_gateway_stage" "databroker_prod_stage" {
   rest_api_id   = "${aws_api_gateway_rest_api.HC_REST_API.id}"
   deployment_id = "${aws_api_gateway_deployment.databroker_prod_deployment.id}"
 }
+
+
+resource "aws_api_gateway_deployment" "databroker_prod_deployment" {
+  depends_on = ["aws_api_gateway_integration.databroker_get_integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.HC_REST_API.id}"
+  stage_name  = "PROD"
+}
