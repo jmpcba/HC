730d27d2764975e7bc7b042daa565263ea549017
Manuel <jmpcba@gmail.com>
2019-08-03 23:31:15 -0300

    typos

--- infra/apiGateway.tf
+++ infra/apiGateway.tf
@@ -63,10 +63,10 @@ resource "aws_api_gateway_resource" "prestador" {
 }
 
 
-resource "aws_api_gateway_method" "prestadores_get_method" {
+resource "aws_api_gateway_method" "prestadores_put_method" {
     rest_api_id          = "${aws_api_gateway_rest_api.HC_REST_API.id}"
     resource_id          = "${aws_api_gateway_resource.prestador.id}"
-    http_method          = "GET"
+    http_method          = "PUT"
     authorization        = "NONE"
   
     request_parameters   = {
@@ -82,12 +82,22 @@ resource "aws_api_gateway_method" "prestadores_post_method" {
     authorization        = "NONE"
 }
 
-resource "aws_api_gateway_integration" "prestadores_get_integration" {
+resource "aws_api_gateway_integration" "prestadores_put_integration" {
+    rest_api_id             = "${aws_api_gateway_rest_api.HC_REST_API.id}"
+    resource_id             = "${aws_api_gateway_resource.prestador.id}"
+    http_method             = "${aws_api_gateway_method.prestadores_put_method.http_method}"
+    content_handling        = "CONVERT_TO_TEXT" 
+    integration_http_method = "POST"
+    type                    = "AWS_PROXY"
+    uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.HC_prestador_service_lambda.arn}/invocations"
+}
+
+resource "aws_api_gateway_integration" "prestadores_post_integration" {
     rest_api_id             = "${aws_api_gateway_rest_api.HC_REST_API.id}"
     resource_id             = "${aws_api_gateway_resource.prestador.id}"
     http_method             = "${aws_api_gateway_method.prestadores_post_method.http_method}"
     content_handling        = "CONVERT_TO_TEXT" 
     integration_http_method = "POST"
     type                    = "AWS_PROXY"
-    uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.HC_prestadores_service_lambda.arn}/invocations"
+    uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.HC_prestador_service_lambda.arn}/invocations"
 }
