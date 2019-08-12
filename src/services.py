7bbdb6be29369f9aab7d9e1c0593f9c4955ba5c2
Manuel <jmpcba@gmail.com>
2019-08-11 22:28:50 -0300

    campo afiliado

--- src/services.py
+++ src/services.py
@@ -204,7 +204,8 @@ class PacientesService(Service):
     # INSERT
     def post(self, new_paciente):
         try:
-            paciente = Paciente(DNI=new_paciente['DNI'], 
+            paciente = Paciente(afiliado=new_paciente['afiliado'], 
+                                DNI=new_paciente['DNI'], 
                                 nombre=new_paciente['nombre'],
                                 apellido=new_paciente['apellido'],
                                 localidad=new_paciente['localidad'],
