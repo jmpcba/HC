1be33e3dc26b4441a2238d47ddf7fce7db107910
Manuel <jmpcba@gmail.com>
2020-04-08 23:48:29 -0300

    docker file for build execution and new rds resource

--- infra/rds.tf
+++ infra/rds.tf
@@ -6,4 +6,12 @@ resource "aws_rds_cluster" "dev_db" {
   database_name           = "hc_rds_db"
   master_username         = "hc_admin"
   master_password         = var.db_password
+
+  scaling_configuration {
+    auto_pause               = true
+    max_capacity             = 1
+    min_capacity             = 1
+    seconds_until_auto_pause = 300
+    timeout_action           = "ForceApplyCapacityChange"
+  }
 }
