b1b447747b51196bd075165aaea02e9f6a42ee83
jupalacios <jupalacios@contractor.ea.com>
2020-04-09 23:56:51 -0300

    use rds version compatible with serverless

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,7 +1,6 @@
 resource "aws_rds_cluster" "dev_db" {
   cluster_identifier      = "hc-rds-dev"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.03.2"
   engine_mode             = "serverless"
   database_name           = "hc_rds_db"
   master_username         = "hc_admin"
